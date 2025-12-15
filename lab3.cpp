e6873ec63579320a6e108445461052b8e85b1828
6804062610215 <s6804062610215@email.kmutnb.ac.th>
2025-12-15 10:01:16 +0700

    Bonus2 lab3.cpp

--- lab3.cpp
+++ lab3.cpp
@@ -6,22 +6,27 @@ int main () {
 	
 	scanf("%d",&score);
 	
-	if (score >= 68) {
-		if (score >= 85){
-			printf("A");
-		}else{
-			if (score >= 75){
-				printf("B");
+	if (score >= 0 && score <= 100){
+		if (score >= 68) {
+			if (score >= 85){
+				printf("A");
 			}else{
-				more = 75 - score;
-				printf("C \nYou Need More %d to B", more);
+				if (score >= 75){
+					printf("B");
+				}else{
+					more = 75 - score;
+					printf("C \nYou Need More %d to B", more);
+				}
+			}
+		} else {
+			if (score >= 55){
+				printf("D");
+			}else{
+				printf("F");
 			}
 		}
-	} else {
-		if (score >= 55){
-			printf("D");
-		}else{
-			printf("F");
-		}
+	}else{
+		printf("error");
 	}
+	
 }
